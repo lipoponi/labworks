eeae76f5fdc3d5593248aabd0827351ccbcd8b30
Stanislav <stas.tebloev@yandex.ru>
2019-05-23 14:33:21 +0400

    done

--- AaDS_labwork2.3/taskE.cpp
+++ AaDS_labwork2.3/taskE.cpp
@@ -3,56 +3,56 @@
 
 using namespace std;
 
-vector<int> u, c, used;
-vector<vector<int>> g, rev;
+
+vector<int> p, cnt;
+vector<bool> used;
+vector<char> color;
+vector<vector<int>> tree, ways;
 int answer = -1;
 
 
-int dfs2(int x) {
-    c[x] = 0;
-    int y = used[x];
+int get(int x) {
+    if (p[x] == x) return x;
+    else return p[x] = get(p[x]);
+}
 
-    for (int next : g[x]) {
-        if (c[next] == 2) {
-            y += dfs2(next);
+void join(int x, int y) {
+    p[get(y)] = get(x);
+}
+
+void dfsPre(int v) {
+    color[v] = 1;
+
+    for (int next : tree[v]) {
+        if (color[next] != 0) continue;
+
+        dfsPre(next);
+        join(v, next);
+    }
+
+    for (int next : ways[v]) {
+        if (color[next] == 2) {
+            int z = get(next);
+            cnt[z] -= 2;
+            cnt[next]++;
+            cnt[v]++;
         }
+    }
+
+    color[v] = 2;
+}
+
+int dfsAns(int x) {
+    used[x] = true;
+    int y = cnt[x];
+
+    for (int next : tree[x]) {
+        if (used[next]) continue;
+        y += dfsAns(next);
     }
 
     answer += (y == 0);
     return y;
-}
-
-int gett(int x) {
-    while (u[x] != x) {
-        u[x] = u[u[x]];
-    }
-    return u[x];
-}
-
-void uni(int x, int i) {
-    u[gett(i)] = gett(x);
-}
-
-void dfs(int v) {
-    c[v] = 1;
-
-    for (int next : g[v]) {
-        if (c[next] != 0) continue;
-
-        dfs(next);
-        uni(v, next);
-    }
-
-    for (int next : rev[v]) {
-        if (c[next] == 2) {
-            int z = gett(next);
-            used[z] -= 2;
-            used[next]++;
-            used[v]++;
-        }
-    }
-
-    c[v] = 2;
 }
 
 
@@ -60,19 +60,20 @@ int main() {
     int n;
     cin >> n;
 
-    g.resize(n);
-    rev.resize(n);
-    u.resize(n);
-    c.resize(n, 0);
-    used.resize(n, 0);
+    tree.resize(n);
+    ways.resize(n);
+    p.resize(n);
+    color.resize(n, 0);
+    used.resize(n, false);
+    cnt.resize(n, 0);
 
     for (int i = 0; i < n - 1; i++) {
         int x, y;
         cin >> x >> y;
         x--, y--;
 
-        g[x].push_back(y);
-        g[y].push_back(x);
+        tree[x].push_back(y);
+        tree[y].push_back(x);
     }
 
     int m;
@@ -83,16 +84,16 @@ int main() {
         cin >> b >> e;
         b--, e--;
 
-        rev[b].push_back(e);
-        rev[e].push_back(b);
+        ways[b].push_back(e);
+        ways[e].push_back(b);
     }
 
     for (int i = 0; i < n; i++) {
-        u[i] = i;
+        p[i] = i;
     }
 
-    dfs(0);
-    dfs2(0);
+    dfsPre(0);
+    dfsAns(0);
 
     cout << answer << endl;
     return 0;
